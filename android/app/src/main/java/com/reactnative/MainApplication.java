6782382aeebc0394db243753f869dd07ee24ee61
tenglai <13049322087@163.com>
2018-07-25 20:15:43 +0800

    react-native link

--- android/app/src/main/java/com/reactnative/MainApplication.java
+++ android/app/src/main/java/com/reactnative/MainApplication.java
@@ -3,6 +3,8 @@ package com.reactnative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactVideoPackage(),
+            new ImagePickerPackage(),
           new VectorIconsPackage()
       );
     }
